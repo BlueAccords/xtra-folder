60d421b5ec31de6df0dc045e7c4cf2fb9a9b02b4
BlueAccords <myoldemailname@gmail.com>
2018-04-06 17:02:51 -0700

    added code to seed chip_Code data for each chip

--- server/db/seeds/master_seed.js
+++ server/db/seeds/master_seed.js
@@ -3,60 +3,89 @@ const userData = require('./data/user_data');
 const folderData = require('./data/folder_data');
 const gameData = require('./data/game_data');
 const chipData = require('./data/chip_data');
+const chipCodeData = require('./data/chip_data_with_codes');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('chip').del()
-    .then(function () {
-      return knex.raw('ALTER TABLE chip AUTO_INCREMENT = 0')
+  .then(function () {
+    return knex.raw('ALTER TABLE chip AUTO_INCREMENT = 0')
+  })
+  .then(function () {
+    return knex('folder').del();
+  })
+  .then(function() {
+    return knex.raw('ALTER TABLE folder AUTO_INCREMENT = 0')
+  })
+  .then(function () {
+    return knex('game').del();
+  })
+  .then(function() {
+    return knex.raw('ALTER TABLE game AUTO_INCREMENT = 0')
+  })
+  .then(function () {
+    return knex('user').del();
+  })
+  .then(function() {
+    return knex.raw('ALTER TABLE user AUTO_INCREMENT = 0')
+  })
+  .then(function () {
+    return knex('chip_copy').del();
+  })
+  .then(function() {
+    return knex.raw('ALTER TABLE chip_copy AUTO_INCREMENT = 0')
+  })
+  .then(function () {
+    return knex('chip_code').del();
+  })
+  .then(function() {
+    return knex.raw('ALTER TABLE chip_code AUTO_INCREMENT = 0')
+  })
+  .then(function() {
+    // Inserts seed entries
+    return knex('user').insert(userData);
+  })
+  .then(function() {
+    return knex('game').insert(gameData.parent_game_data());
+  })
+  .then(function() {
+    return knex.select('id').from('game').first().then(function(firstGame) {
+      return knex('game').insert(gameData.child_game_data(firstGame));
     })
-    .then(function () {
-      return knex('folder').del();
-    })
-    .then(function() {
-      return knex.raw('ALTER TABLE folder AUTO_INCREMENT = 0')
-    })
-    .then(function () {
-      return knex('game').del();
-    })
-    .then(function() {
-      return knex.raw('ALTER TABLE game AUTO_INCREMENT = 0')
-    })
-    .then(function () {
-      return knex('user').del();
-    })
-    .then(function() {
-      return knex.raw('ALTER TABLE user AUTO_INCREMENT = 0')
-    })
-    .then(function () {
-      return knex('chip_copy').del();
-    })
-    .then(function() {
-      return knex.raw('ALTER TABLE chip_copy AUTO_INCREMENT = 0')
-    })
-    .then(function() {
-      // Inserts seed entries
-      return knex('user').insert(userData);
-    })
-    .then(function() {
-      return knex('game').insert(gameData.parent_game_data());
-    })
-    .then(function() {
-      return knex.select('id').from('game').first().then(function(firstGame) {
-        return knex('game').insert(gameData.child_game_data(firstGame));
-      })
-    })
-    .then(function() {
-      return knex.select('id').from('user').first().then(function(oneUser) {
-        return knex('folder').insert(folderData(oneUser));
+  })
+  .then(function() {
+    return knex.select('id').from('user').first().then(function(oneUser) {
+      return knex('folder').insert(folderData(oneUser));
+    });
+  })
+  .then(function() {
+    chipData.forEach(function(chip, index, arr) {
+      chipData[index].primary_game_id = 1; 
+    });
+    return knex('chip').insert(chipData);
+  })
+  .then(function() {
+    return knex.select('*').from('chip').then(function(firstTenChips) {
+      // create list of valid chip_code objects
+      listOfCodes = [];
+      listOfChipCodeObj = [];
+      chipCodeData.forEach(function(chip, index, arr) {
+        const chipName = chipCodeData[index].original_name;
+        const chipCodes = chipCodeData[index]["Code(s)"];
+        const codesList = chipCodes.split(",");
+        const codeObj = codesList.map(obj => {
+          let rObj = {};
+          rObj.code = obj.trim();
+          rObj.chip_id = firstTenChips[index].id
+          
+          return rObj;
+        });
+        listOfCodes = listOfCodes.concat(codeObj);
       });
-    })
-    .then(function() {
-      chipData.forEach(function(chip, index, arr) {
-       chipData[index].primary_game_id = 1; 
-      });
-
-      return knex('chip').insert(chipData);
-    })
-
+      // console.log(listOfCodes);
+      
+      return knex('chip_code').insert(listOfCodes);
+    });
+  })
+  
 };
