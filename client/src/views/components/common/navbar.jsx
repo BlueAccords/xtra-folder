be39847c6ccd439d215be61a041c1565a8c6205e
BlueAccords <myoldemailname@gmail.com>
2018-04-25 14:03:09 -0700

    abstracted navbar into its own component

---
+++ client/src/views/components/common/navbar.jsx
@@ -0,0 +1,40 @@
+// reusable navbar for header
+
+import React from 'react';
+import { Link, Route } from 'react-router-dom'
+
+const NavBar = (props) => {
+  const { isActive, toggleActive } = props;
+  return (
+       <nav className="navbar has-shadow" role="navigation" aria-label="main navigation">
+       <div className="container">
+         <div className="navbar-brand">
+          <a href="#" className="navbar-item">
+            Xtra-Folder
+          </a>
+
+          {/* hamburger button*/}
+          <div role="button" className={`navbar-burger ${isActive ? 'is-active' : null}`} 
+            aria-label="menu" aria-expanded="false"
+            onClick={toggleActive}>
+            <span aria-hidden="true"></span> 
+            <span aria-hidden="true"></span> 
+            <span aria-hidden="true"></span> 
+          </div> 
+        </div>
+
+        {/* desktop menu, is hidden by default on mobile */}
+        <div className={`navbar-menu ${isActive ? 'is-active' : null}`}
+          role="navigation" aria-label="main navigation">
+          <div className="navbar-end">
+            <Link to="/" className="navbar-item">Home</Link>
+            <Link to="/about" className="navbar-item">About</Link>
+            <Link to="/contact" className="navbar-item">Contact</Link>
+          </div>
+        </div>      
+       </div>
+      </nav>
+  )
+}
+
+export default NavBar;
--- client/src/views/layouts/main.jsx
+++ client/src/views/layouts/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
+
+import NavBar from './../components/common/navbar.jsx';
 import HomePage from './../pages/home.jsx';
 import AboutPage from './../pages/about.jsx';
 import ContactPage from './../pages/contact.jsx';
@@ -12,11 +14,11 @@ class MainLayout extends React.Component {
       isActive: false,
     };
 
-    this.toggleClass = this.toggleClass.bind(this);
+    this.toggleActive = this.toggleActive.bind(this);
   }
 
   // toggle mobile menu
-  toggleClass() {
+  toggleActive() {
     let currentState = this.state.isActive;
     this.setState({ isActive: !currentState });
   }
@@ -24,34 +26,7 @@ class MainLayout extends React.Component {
     const isActiveClassName = this.state.isActive ? 'is-active' : '';
     return (
       <div>
-       <nav className="navbar has-shadow" role="navigation" aria-label="main navigation">
-       <div className="container">
-         <div className="navbar-brand">
-          <a href="#" className="navbar-item">
-            Xtra-Folder
-          </a>
-
-          {/* hamburger button*/}
-          <div role="button" className={`navbar-burger ${this.state.isActive ? 'is-active' : null}`} 
-            aria-label="menu" aria-expanded="false"
-            onClick={this.toggleClass}>
-            <span aria-hidden="true"></span> 
-            <span aria-hidden="true"></span> 
-            <span aria-hidden="true"></span> 
-          </div> 
-        </div>
-
-        {/* desktop menu, is hidden by default on mobile */}
-        <div className={`navbar-menu ${this.state.isActive ? 'is-active' : null}`}
-          role="navigation" aria-label="main navigation">
-          <div className="navbar-end">
-            <Link to="/" className="navbar-item">Home</Link>
-            <Link to="/about" className="navbar-item">About</Link>
-            <Link to="/contact" className="navbar-item">Contact</Link>
-          </div>
-        </div>      
-       </div>
-      </nav>     
+        <NavBar isActive={this.state.isActive} toggleActive={this.toggleActive} />
         <Route exact path="/" component={HomePage}/>
         <Route path="/about" component={AboutPage}/>
         <Route path="/contact" component={ContactPage}/>
